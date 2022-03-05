8f642f2511e971fc65de70d090995f66a864fa34
Michael <michael092001@gmail.com>
2022-03-05 16:55:46 -0500

    accounts attempt register

--- P2/accounts/urls.py
+++ P2/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .views import RegisterView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
