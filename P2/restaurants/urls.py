e92fd22f9cc210314b8e2c54525f0a12a12f85b3
Maor Gornic <maor.gornic@mail.utoronto.ca>
2022-03-06 03:25:33 +0000

    Finished edit endpoint for menu items

--- P2/restaurants/urls.py
+++ P2/restaurants/urls.py
@@ -6,5 +6,5 @@ app_name = 'restaurants'
 
 urlpatterns = [
     path('<int:restaurant_id>/menu/new/', CreateMenuItem.as_view(), name='create-menuitem'),
-    path('<int:restaurant_id>/menu/<int:menu_item>/edit/', UpdateMenuItem.as_view(), name='update-menuitem'),
+    path('<int:restaurant_id>/menu/<int:pk>/edit/', UpdateMenuItem.as_view(), name='update-menuitem'),
 ]
--- P2/restaurants/views.py
+++ P2/restaurants/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404, JsonResponse
 from django.shortcuts import render
 from rest_framework.generics import get_object_or_404, CreateAPIView, UpdateAPIView
-from restaurants.models import Restaurant
+from restaurants.models import MenuItem, Restaurant
 from restaurants.serializers import MenuItemSerializer
 from rest_framework.permissions import IsAuthenticated
 
@@ -16,17 +16,36 @@ class CreateMenuItem(CreateAPIView):
         except Http404:
             return JsonResponse({"detail": "Restaurant not found"}, status=404)
 
-        return super().dispatch(request, *args, **kwargs)
+        # Getting JWT token from user
+        ret = super().dispatch(request, *args, **kwargs)
+
+        if self.request.user.id != self.restaurant.owner_id:
+            return JsonResponse({"detail": "Not owner of restaurant"}, status=403)
+        return ret
 
     def perform_create(self, serializer):
-        print(self.request.user)
         return serializer.save(restaurant=self.restaurant)
 
 
 
 class UpdateMenuItem(UpdateAPIView):
+    queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
     permission_classes = [IsAuthenticated]
+
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            self.restaurant = get_object_or_404(Restaurant, id=self.kwargs['restaurant_id'])
+        except Http404:
+            return JsonResponse({"detail": "Restaurant not found"}, status=404)
+
+        # Getting JWT token from user
+        ret = super().dispatch(request, *args, **kwargs)
+        
+        if self.request.user.id != self.restaurant.owner_id:
+            return JsonResponse({"detail": "Not owner of restaurant"}, status=403)
+        return ret
+
 
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
