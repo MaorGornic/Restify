27bff03d9954aca6d457e3d45a6c4d6dccf5f717
Michael <michael092001@gmail.com>
2022-03-12 15:52:13 -0500

    Add pagination

--- P2/Restify/settings.py
+++ P2/Restify/settings.py
@@ -132,6 +132,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
