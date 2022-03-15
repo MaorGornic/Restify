55c032c6682d13e41b2c3b28ae060452306535ee
Michael <michael092001@gmail.com>
2022-03-15 12:25:21 -0400

    Add view notification endpoint

--- P2/accounts/views.py
+++ P2/accounts/views.py
@@ -1,6 +1,7 @@
+from django.http import Http404, JsonResponse
 from rest_framework import generics
 from django.contrib.auth.models import User
-from rest_framework.generics import RetrieveAPIView, get_object_or_404
+from rest_framework.generics import RetrieveAPIView, get_object_or_404, UpdateAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
@@ -40,6 +41,28 @@ class APIUserView(RetrieveAPIView):
 
     def get_object(self):
         return get_object_or_404(ModifiedUser, id=self.request.user.id)
+
+
+class MarkViewedNotification(UpdateAPIView):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationRecordsSerializer
+    permission_classes = [IsAuthenticated]
+
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            self.notification = get_object_or_404(Notification, id=self.kwargs['notification_id'])
+        except Http404:
+            return JsonResponse({"detail": "Notification not found"}, status=404)
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        self.kwargs['pk'] = self.kwargs['notification_id']
+        return super().update(request, *args, **kwargs)
+
+    def perform_update(self, serializer):
+        serializer.validated_data.update({'viewed': True})
+        return super().perform_update(serializer)
 
 
 class NotificationView(generics.ListAPIView):
