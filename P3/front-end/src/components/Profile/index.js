5976be2dd66aadc48dc5f39a5f09675598036836
Richard Lu <richardlu0928@gmail.com>
2022-04-16 18:39:49 -0400

    Added endpoints for profiles.

---
+++ P3/front-end/src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios";
+import { useMemo, useState, useEffect, useLocation } from "react";
+import { Box, Flex, Heading, Spacer, Text, Center, Square } from "@chakra-ui/react";
+
+const Profile = () => {
+    console.log("pass");
+}
+
+export default Profile;
---
+++ P3/front-end/src/components/ProfileEdit/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios";
+import { useMemo, useState, useEffect, useLocation } from "react";
+import { Box, Flex, Heading, Spacer, Text, Center, Square } from "@chakra-ui/react";
+
+const ProfileEdit = () => {
+    console.log("edit");
+}
+
+export default ProfileEdit;
