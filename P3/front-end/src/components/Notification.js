fa3b2af867c36211f8928825078457d13dda347a
Michael <michael092001@gmail.com>
2022-04-16 19:23:12 -0400

    Basic notification fetch

--- P3/front-end/src/components/Notification.js
+++ P3/front-end/src/components/Notification.js
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+// import Box ui component?
+import { Box} from "@chakra-ui/react";
 
 import {
   Popover,
@@ -21,8 +24,42 @@ import {
 import { FaBell } from "react-icons/fa";
 
 function Notification({ style }) {
+  
+  const [currentPage, setCurrentPage] = useState(1);
+  const [notifReq, setNotifReq] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const navigate = useNavigate();
 
+  
+
+  const getNotifications = (searchUrl) => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
+      },
+    };
+
+    setLoading(true);
+    axios
+      .get(searchUrl, config)
+      .then((res) => {
+        setNotifReq(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        // TODO
+      });
+  };
+
+  useEffect(() => { 
+    if (currentPage) {
+      getNotifications(`http://127.0.0.1:8000/accounts/notifications/?page=${currentPage}`);
+    } else {
+      getNotifications(`http://127.0.0.1:8000/accounts/notifications/`);
+  }}, [currentPage]);
+
+  // Return the results returned by the notification request inside a popover table 
   return (
     <Popover>
       <PopoverTrigger>
@@ -36,10 +73,27 @@ function Notification({ style }) {
         />
       </PopoverTrigger>
       <PopoverContent>
+        <PopoverHeader>Notifications</PopoverHeader>
         <PopoverArrow />
         <PopoverCloseButton />
-        <PopoverHeader>Notifications</PopoverHeader>
-        <PopoverBody>Wendy's added a new item to their menu</PopoverBody>
+        
+        {loading ? (
+          <Center>
+          </Center>
+        ) : (
+          <Box>
+            {notifReq.count > 0 &&
+              notifReq.results.map((notif) => (
+                // create PopoverBody element with notification details
+                <PopoverBody>
+                  <VStack>
+                    <Text fontSize="sm">{notif.type}</Text>
+                    <Text fontSize="sm">{notif.created_at}</Text>
+                  </VStack>
+                </PopoverBody>
+              ))}
+          </Box>
+        )}
       </PopoverContent>
     </Popover>
   );
