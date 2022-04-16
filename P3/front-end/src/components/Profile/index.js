c7c1d113b9285e9e9e2680afcde4778a704945df
Richard Lu <richardlu0928@gmail.com>
2022-04-16 18:52:09 -0400

    Added NavBars Redirection.

--- P3/front-end/src/components/Profile/index.js
+++ P3/front-end/src/components/Profile/index.js
@@ -3,9 +3,17 @@ import { Link, useNavigate } from 'react-router-dom'
 import axios from "axios";
 import { useMemo, useState, useEffect, useLocation } from "react";
 import { Box, Flex, Heading, Spacer, Text, Center, Square } from "@chakra-ui/react";
+import MainNavBar from "../MainNavBar";
 
 const Profile = () => {
     console.log("pass");
+    return (
+        <Box>
+            <MainNavBar>
+                <h3>Hello</h3>
+            </MainNavBar>
+        </Box>
+    );
 }
 
 export default Profile;
--- P3/front-end/src/components/ProfileEdit/index.js
+++ P3/front-end/src/components/ProfileEdit/index.js
@@ -3,9 +3,17 @@ import { Link, useNavigate } from 'react-router-dom'
 import axios from "axios";
 import { useMemo, useState, useEffect, useLocation } from "react";
 import { Box, Flex, Heading, Spacer, Text, Center, Square } from "@chakra-ui/react";
+import MainNavBar from "../MainNavBar";
 
 const ProfileEdit = () => {
     console.log("edit");
+    return (
+        <Box>
+            <MainNavBar>
+                <h3>Hello</h3>
+            </MainNavBar>
+        </Box>
+    );
 }
 
 export default ProfileEdit;
